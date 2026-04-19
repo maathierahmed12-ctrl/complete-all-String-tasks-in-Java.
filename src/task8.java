39492bb1bbb884c0096ff57636208e5ccd0bf069
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-19 17:03:17 +0400

    Task9

---
+++ src/task8.java
@@ -0,0 +1,21 @@
+public class Task8 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean Task8(String str) {
+
+        int max;
+        int charAt = 'a';
+        int charAt = 'b';
+        int result = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+              result += str.charAt(i);
+        }
+             return result;
+
+            }
+
+        }
