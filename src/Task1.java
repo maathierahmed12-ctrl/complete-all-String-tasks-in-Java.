66a43caf6aade068a0fa8d40ad8d97d721f3892d
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-19 14:58:49 +0400

    Task1 each character is repeated twice

---
+++ src/Task1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Task1 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static String doubleChar(String str) {
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            result = ch + result;
+        }
+        return result;
+    }
+
+}
+
