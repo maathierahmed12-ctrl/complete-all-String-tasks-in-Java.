e1869a71781591444c1ede2b309bad525f6c0f6b
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-19 16:17:36 +0400

    Task6

--- src/task6.java
+++ src/task6.java
@@ -4,12 +4,19 @@ public class Task5 {
 
     }
 
-    public static boolean Task5(String str) {
+    public static boolean Task6(String str) {
 
-        int str1;
+        int str1 ;
         int str2;
-        if (str1.end(str2) || str2.end(str1)) {
+
+        for (int i = 0; i <= str.length()-3; i++) {
+
+        }
+
+        if (i==0 || str(i-1)!="xyz") {
+
             return true;
+
         } else {
             return false;
         }
