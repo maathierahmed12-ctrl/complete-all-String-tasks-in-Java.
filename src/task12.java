feb83a33985fd0a16e5ef8d7a88c1560e3834ffd
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-20 09:03:26 +0400

    Task12

---
+++ src/task12.java
@@ -0,0 +1,19 @@
+public class task12 {
+
+        public static void main(String[] args) {
+
+        }
+
+        public static boolean Task12(String word , String sep) {
+
+            String result = "";
+
+            for (int i = n; i > 0; i++) {
+
+                result += word + sep
+
+            }
+            return result;
+        }
+
+    }
