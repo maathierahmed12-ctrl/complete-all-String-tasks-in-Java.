cc7106124898f1ea1c4561bedfdbf785daff69c9
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-20 10:31:57 +0400

    Task20

---
+++ src/task20.java
@@ -0,0 +1,24 @@
+public class task20 {
+    public static boolean Task20(String word, String str) {
+
+        String result = "";
+
+        for (int i = str.charAt='+'; i > 0; i++) {
+
+            if (str.charAt(i) == '+') {
+
+
+                result += word;
+
+            }
+        }
+        for (int i = str.charAt='+'; i > 0; i--) {
+
+            if (str.charAt(i) == '+') {
+
+                result += word;
+            }
+
+
+
+
