6dc9e4bb3303ad18b8c16a287cdb63418a5a4c3c
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-20 09:39:25 +0400

    Task18

--- src/task17.java
+++ src/task17.java
@@ -1,2 +1,16 @@
 public class task17 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String Task17(String str, String charAt) {
+        String result = "";
+        for (int i = 0; i < str.length() - 2; i += 3) {
+        }
+        return result;
+
+    }
 }
+
+
+
---
+++ src/task18.java
@@ -0,0 +1,25 @@
+public class task18 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String Task17(String str, String charAt) {
+        String result = "";
+        int i;
+
+        for (int i = 0; i < str.length() ; i++) {
+        }
+        return + = str.charAt(i);
+
+        if (str.charAt(i) == 'z' && i + 2 < str.length() && str.charAt(i+2) == 'p') {
+            result += str.charAt(i+2);
+            i += 2;
+        }
+    }
+           return result;
+}
+
+
+
+
