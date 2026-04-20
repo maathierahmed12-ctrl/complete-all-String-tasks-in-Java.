031944ecd1eaf7f446dacaa084db32b52f2593b7
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-20 08:59:12 +0400

    Task11

---
+++ src/task11.java
@@ -0,0 +1,15 @@
+public class Task11 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean Task11(String str int n) {
+
+        String result = "";
+
+    for (int i = n; i > 0; i--) {
+
+    }
+    return result;
+}
+
