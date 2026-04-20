5cb75280f1bf8bd2c860ba0e93439c954839532d
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-20 09:09:55 +0400

    Task13

--- src/task13.java
+++ src/task13.java
@@ -1,2 +1,22 @@
 public class task13 {
-}
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean Task13(String n , String str) {
+
+        String result = "";
+
+        for (int i = n; str >length(); i++) {
+
+            if(str.length() < n) {
+
+                if (str == "n") {
+
+                    return true;
+                } else {
+                    return false;
+
+                }
+            }
