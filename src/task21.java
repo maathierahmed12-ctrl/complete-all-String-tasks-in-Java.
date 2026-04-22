b6027a7ed1c9841dcc61a5dbed40791d6a84adca
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-22 11:58:54 +0400

    Task1doubleChar

--- src/task21.java
+++ src/task21.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class task21 {
+    public static boolean Task21(String word, String str) {
+
+        String result = "";
+
+        for (int i = str.charAt = '+'; i > 0; i++) {
+
+
+            result += word;
+
+        }
+    }
 }
+
