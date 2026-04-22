75c31f50b269f335ef87b5234c8856c0571afbe9
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-22 12:10:17 +0400

    Task10repeatEnd

--- src/repeatEnd.java
+++ src/repeatEnd.java
@@ -1,10 +1,10 @@
-public class Task11 {
+public class repeatEnd {
     public static void main(String[] args) {
 
     }
 
-    public static boolean Task11(String str int n) {
-
+    public static boolean repeatEnd(String str ) {
+        int n;
         String result = "";
 
     for (int i = n; i > 0; i--) {
