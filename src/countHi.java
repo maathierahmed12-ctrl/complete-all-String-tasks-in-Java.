f4c250042b5a09a01276f508a37e290eb75afff9
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-22 12:00:00 +0400

    Task1doubleChar

--- src/countHi.java
+++ src/countHi.java
@@ -1,5 +1,4 @@
-    import java.util.*;
-    public class Task2 {
+public class Task2 {
         public static void main(String[] args) {
 
 
--- src/doubleChar.java
+++ src/doubleChar.java
@@ -1,4 +1,4 @@
-public class Task1 {
+public class doubleChar {
     public static void main(String[] args) {
 
 
